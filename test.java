23d4395467f895678219d0641ded87eacfae4b22
lejos6 FB18 <Lejos6@FB18.POOL.E-TECHNIK.TU-DARMSTADT.DE>
2019-05-22 16:42:46 +0200

    test

---
+++ test.java
@@ -0,0 +1,19 @@
+import lejos.hardware.motor.Motor;
+import lejos.hardware.Button;
+import lejos.hardware.lcd.LCD;
+import lejos.utility.Delay;
+/**
+  *
+  * Beschreibung
+  *
+  * @version 1.0 vom 12.02.2019
+  * @author 
+  */
+
+public class test {
+  
+  public static void main(String[] args) {
+          LCD.drawString("test",1,1);
+          Delay.msDelay(1500);
+  } // end of main
+} // end of class test
